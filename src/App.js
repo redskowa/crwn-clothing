4c52488fbb327c05710e0ee2f405af511da48274
redskowa <redskowa@gmail.com>
2021-11-02 13:57:21 -0600

    updating with a shop page, collection preview, and collection items

--- src/App.js
+++ src/App.js
@@ -3,21 +3,15 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component.jsx";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
-      {/* <HomePage /> */}
     </div>
   );
 }
